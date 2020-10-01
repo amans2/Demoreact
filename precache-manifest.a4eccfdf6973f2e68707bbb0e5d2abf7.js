5e5ee712b5ee654a27d875a65b73c6a0ffbfba55
aman_saxena <aman.saxena_cs15@gla.ac.in>
2020-10-01 17:01:18 +0530

    Updates

---
+++ precache-manifest.a4eccfdf6973f2e68707bbb0e5d2abf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563ab80ff5a72e4151f57587190a21da",
+    "url": "/Demoreact/index.html"
+  },
+  {
+    "revision": "8af75f7bc23d74f277f4",
+    "url": "/Demoreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdcab2610e626414cde3",
+    "url": "/Demoreact/static/js/2.067f0017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demoreact/static/js/2.067f0017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af75f7bc23d74f277f4",
+    "url": "/Demoreact/static/js/main.1782885a.chunk.js"
+  },
+  {
+    "revision": "67d45cbeb49d64f5ff2a",
+    "url": "/Demoreact/static/js/runtime-main.37fa06a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Demoreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
